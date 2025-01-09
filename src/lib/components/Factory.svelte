112b596af8915eb27fedd87066aae0dee1578c22
inorganik <jamie.perkins@gmail.com>
2025-01-09 07:01:49 -0700

    initial commit

---
+++ src/lib/components/Factory.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+
+  import { Application } from '@splinetool/runtime';
+  import { onMount } from 'svelte';
+  import scene from '$lib/spline/scene.splinecode';
+
+  let canvasEl = $state<HTMLCanvasElement>()
+  let splineApp = $state<Application>() 
+
+  onMount(() => {
+    if (canvasEl) {
+      splineApp = new Application(canvasEl);
+      splineApp.load(scene)
+    }
+  });
+</script>
+
+<div class="m-auto w-[410px] h-[410px] border-2 border-black">
+  <canvas class="w-full h-full" bind:this={canvasEl}></canvas>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+
+  import Factory from '$lib/components/Factory.svelte';
+
+</script>
+
+<div class="w-full h-full flex items-center justify-center m-auto">
+  <Factory />
+  <Factory />
+</div>
